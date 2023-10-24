b18941cb9cefbbe9c854cca53a91d6c91d5750ee
tengzhangs <147488840+tengzhangs@users.noreply.github.com>
2023-10-24 20:52:34 +0800

    Update peak_calling.R

--- R/peak_calling.R
+++ R/peak_calling.R
@@ -1,1 +1,27 @@
 library(exomePeak2)
+library(GenomicFeatures)
+samplenames <- c("NA18486","NA18498","NA18499","NA18501","NA18502","NA18504","NA18505","NA18507","NA18508",
+                 "NA18510","NA18511","NA18516","NA18517","NA18519","NA18522","NA18523","NA18852","NA18855",
+                 "NA18856","NA18858","NA18861","NA18862","NA18870","NA18907","NA18909","NA18912","NA18913",
+                 "NA18916","NA19092","NA19093","NA19098","NA19099","NA19101","NA19102","NA19108","NA19114",
+                 "NA19116","NA19119","NA19127","NA19128","NA19130","NA19131","NA19137","NA19138","NA19140",
+                 "NA19143","NA19144","NA19147","NA19152","NA19153","NA19159","NA19160","NA19192","NA19193",
+                 "NA19200","NA19204","NA19209","NA19222","NA19239","NA19257")
+Input_data <- vector()
+for(i in 1:length(samplenames)){
+  Input_data[i] <- paste0("./Lymphoblastoid_cell_line/",samplenames[i],"/",samplenames[i],"Aligned.sortedByCoord.out.bam")
+}
+
+IP_data <- vector()
+for(i in 1:length(samplenames)){
+  IP_data[i] <- paste0("./Lymphoblastoid_cell_line/IP_samples/",samplenames[i],"_IPAligned.sortedByCoord.out.bam")
+}
+
+GENE_ANNO_GTF = "./hg19_GTF/genes.gtf"
+IP_BAM <- c(IP_data)
+INPUT_BAM <- c(Input_data)
+result =exomePeak2(bam_ip = IP_BAM,
+           bam_input = INPUT_BAM,
+           gff_dir = GENE_ANNO_GTF,
+           genome = "hg19",
+           paired_end = FALSE)
